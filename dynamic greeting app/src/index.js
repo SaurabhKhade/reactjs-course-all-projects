a9715dfecaa7ffa6fff8d4d8d00958fa25be50cb
Saurabh Khade <khadesaurabh056@gmail.com>
2020-12-03 09:56:41 +0530

    greeting app added

---
+++ dynamic greeting app/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+let time=new Date(20,13,12,20).getHours();
+
+let greetColor={};
+let greet='';
+
+if (time>=3 && time<12){
+	greetColor.color='rgb(243,148,0)';
+	greet="Good Morning";
+} else if (time>=12 && time<16){
+	greetColor.color='rgb(255,196,38)';
+	greet="Good Afternoon";
+} else if (time>=16 && time<24){
+	greetColor.color='rgb(164,255,234)';
+	greet="Good Evening";
+} else{
+	greetColor.color='rgb(255,170,236)';
+	greet="Good Night";
+}
+
+
+ReactDOM.render(
+	<div className='mainbox'>
+		<div className='subbox'>
+			<h1>Hello Sir, <span style={greetColor}>{greet}</span></h1>
+		</div>
+	</div>,
+  document.getElementById('root')
+);
